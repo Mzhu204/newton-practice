1fbc5663f109941476e6d3f110f81114b6e0a075
Michelle Zhu <michelle_zhu@berkeley.edu>
2026-08-20 10:18:21 -0700

    multivariate version of the newton optimization function

---
+++ multivariate_newton.py
@@ -0,0 +1,18 @@
+from scipy.optimize import minimize
+
+x= lambda x: (x**2)+2
+
+
+ef rosen_hess(x):
+    x = np.asarray(x)
+    H = np.diag(-400*x[:-1],1) - np.diag(400*x[:-1],-1)
+    diagonal = np.zeros_like(x)
+    diagonal[0] = 1200*x[0]**2-400*x[1]+2
+    diagonal[-1] = 200
+    diagonal[1:-1] = 202 + 1200*x[1:-1]**2 - 400*x[2:]
+    H = H + np.diag(diagonal)
+    return H
+
+res = minimize(rosen, x0, method='Newton-CG',
+               jac=rosen_der, hess=rosen_hess,
+               options={'xtol': 1e-8, 'disp': True})
